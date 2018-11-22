d50ed394ba8784e08a6edc72fa83ff0436116c8e
maniCreate <aion1998@yahoo.co.jp>
2018-11-23 01:59:06 +0900

    Initial Commit

---
+++ mana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mana
+//
+//  Created by Syunsuke Nakao on 2018/11/23.
+//  Copyright © 2018 Syunsuke Nakao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
